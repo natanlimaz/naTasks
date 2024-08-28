532552e4e9d778fbda5a3e4467dc9045e04004d5
natanlimaz <natanlimadev@gmail.com>
2024-08-28 00:42:05 -0300

    feat: Criação do component header e da página dashboard

---
+++ src/components/header/index.tsx
@@ -0,0 +1,28 @@
+import styles from "./styles.module.css";
+import Link from "next/link";
+
+export function Header() {
+
+  return (
+    <header className={styles.header}>
+        <section className={styles.content}>
+            <nav className={styles.nav}>
+							<Link href="/">
+								<h1 className={styles.logo}>
+									Na
+									<span>(</span>
+									Tasks<span>)</span>
+									<span>+</span>
+								</h1>	
+							</Link>
+
+							<Link href="/dashboard" className={styles.link}>
+								Meu painel
+							</Link>
+            </nav>
+
+						<button className={styles.loginButton}>Acessar</button>
+        </section>
+    </header>
+  )
+}
